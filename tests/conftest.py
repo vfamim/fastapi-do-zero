d2fa5cac8a6ffc24383c41c04b04314c28b8aa95
vfamim <vfamim@gmail.com>
2023-12-12 20:34:21 -0300

    Adicionada a primeira migração Alembic. Criada tabela de usuários.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,20 @@
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from fastapi.testclient import TestClient
 from fast_zero.app import app
+from fast_zero.models import Base
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    yield Session()
+    Base.metadata.drop_all(engine)
