d6a85226c05cc47d3d03cd5066972ec055c19bf1
vfamim <vfamim@gmail.com>
2023-12-12 21:35:26 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+from fast_zero.schemas import UserPublic
+
+
 def test_root_deve_retornar_200_e_ola_mundo(client):
     response = client.get('/')
     assert response.status_code == 200
@@ -24,15 +27,7 @@ def test_create_user(client):
 def test_read_user(client):
     response = client.get('/users/')
     assert response.status_code == 200
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
 def test_update_user(client):
@@ -57,3 +52,9 @@ def test_delete_user(client):
 
     assert response.status_code == 200
     assert response.json() == {'detail': 'User deleted'}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
