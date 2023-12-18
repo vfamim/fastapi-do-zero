fe3813e84fe0b6e5da367e83852344ec1e1a9a9f
vfamim <vfamim@gmail.com>
2023-12-18 01:00:45 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from fastapi.testclient import TestClient
 from fast_zero.app import app
 from fast_zero.models import Base, User
 from fast_zero.database import get_session
+from fast_zero.security import get_password_hash
 
 
 @pytest.fixture
@@ -35,9 +36,24 @@ def client(session):
 
 @pytest.fixture
 def user(session):
-    user = User(username='Teste', email='teste@test.com', password='testtest')
+    user = User(
+        username='Teste',
+        email='teste@test.com',
+        password=get_password_hash('testtest'),
+    )
     session.add(user)
     session.commit()
     session.refresh(user)
 
+    user.clean_password = 'testtest'
+
     return user
+
+
+@pytest.fixture
+def token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    return response.json()['access_token']
--- tests/test_app.py
+++ tests/test_app.py
@@ -24,15 +24,34 @@ def test_create_user(client):
     }
 
 
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == 200
+    assert 'access_token' in token
+    assert 'token_type' in token
+
+
 def test_read_user(client):
     response = client.get('/users/')
     assert response.status_code == 200
     assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'bob',
             'email': 'bob@example.com',
@@ -47,14 +66,10 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
-    response = client.delete('/users/1')
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}', headers={'Authorization': f'Bearer {token}'}
+    )
 
     assert response.status_code == 200
     assert response.json() == {'detail': 'User deleted'}
-
-
-def test_read_users_with_users(client, user):
-    user_schema = UserPublic.model_validate(user).model_dump()
-    response = client.get('/users/')
-    assert response.json() == {'users': [user_schema]}
