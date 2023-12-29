5098ba3a89467a52effde65626d61541a7015ba4
vfamim <vfamim@gmail.com>
2023-12-29 12:48:20 -0300

    Implement DELETE and PATCH endpoints for todos

--- fast_zero/routes/todos.py
+++ fast_zero/routes/todos.py
@@ -1,11 +1,12 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import Todo, User
-from fast_zero.schemas import TodoPublic, TodoSchema
+from fast_zero.schemas import TodoPublic, TodoSchema, TodoList
 from fast_zero.security import get_current_user
 
 router = APIRouter()
@@ -47,7 +48,10 @@ def list_todos(
     query = select(Todo).where(Todo.user_id == user.id)
 
     if title:
-        query = query.filter(Todo.title.contains(description))
+        query = query.filter(Todo.title.contains(title))
+
+    if description:
+        query = query.filter(Todo.description.contains(description))
 
     if state:
         query = query.filter(Todo.state == state)
--- tests/test_todos.py
+++ tests/test_todos.py
@@ -1,3 +1,8 @@
+import factory.fuzzy
+
+from fast_zero.models import Todo, TodoState
+
+
 def test_create_todo(client, token):
     response = client.post(
         '/todos/',
@@ -14,3 +19,109 @@ def test_create_todo(client, token):
         'description': 'Test todo description',
         'state': 'draft',
     }
+
+
+class TodoFactory(factory.Factory):
+    class Meta:
+        model = Todo
+
+    title = factory.Faker('text')
+    description = factory.Faker('text')
+    state = factory.fuzzy.FuzzyChoice(TodoState)
+    user_id = 1
+
+
+def test_list_todos(session, client, user, token):
+    session.bulk_save_objects(TodoFactory.create_batch(5, user_id=user.id))
+    session.commit()
+
+    response = client.get(
+        '/todos/',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['todos']) == 5
+
+
+def test_list_todos_pagination(session, user, client, token):
+    session.bulk_save_objects(TodoFactory.create_batch(5, user_id=user.id))
+    session.commit()
+
+    response = client.get(
+        '/todos/?offset=1&limit=2',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['todos']) == 2
+
+
+def test_list_todos_filter_title(session, user, client, token):
+    session.bulk_save_objects(
+        TodoFactory.create_batch(5, user_id=user.id, title='Test todo 1')
+    )
+    session.commit()
+
+    response = client.get(
+        '/todos/?title=Test todo 1',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['todos']) == 5
+
+
+def test_list_todos_filter_description(session, user, client, token):
+    session.bulk_save_objects(
+        TodoFactory.create_batch(5, user_id=user.id, description='description')
+    )
+    session.commit()
+
+    response = client.get(
+        '/todos/?description=desc',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['todos']) == 5
+
+
+def test_list_todos_filter_state(session, user, client, token):
+    session.bulk_save_objects(
+        TodoFactory.create_batch(5, user_id=user.id, state=TodoState.draft),
+    )
+    session.commit()
+
+    response = client.get(
+        '/todos/?state=draft',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['todos']) == 5
+
+
+def test_list_todos_filter_combined(session, user, client, token):
+    session.bulk_save_objects(
+        TodoFactory.create_batch(
+            5,
+            user_id=user.id,
+            title='Test todo combined',
+            description='combined description',
+            state=TodoState.done,
+        )
+    )
+
+    session.bulk_save_objects(
+        TodoFactory.create_batch(
+            3,
+            user_id=user.id,
+            title='Other title',
+            description='Other description',
+            state=TodoState.todo,
+        )
+    )
+    session.commit()
+
+    response = client.get(
+        '/todos/?title=Test todo combined&description=combined&state=done',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert len(response.json()['todos']) == 5
